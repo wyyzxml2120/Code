6119ed262686e1361c492fdfbabc8bfb05e89de1
wyyzxml <447942212@qq.com>
2020-10-10 15:48:30 +0800

    添加后台

--- app/src/main/java/com/example/code/MainActivity.java
+++ app/src/main/java/com/example/code/MainActivity.java
@@ -12,11 +12,18 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.code.data.CodeBean;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.SaveListener;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
@@ -26,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Bmob.initialize(this, "cfa78f7cfb103da7878d6047f3478173");
+
         textView = findViewById(R.id.text);
         Button btnCopy = findViewById(R.id.btnCopy);
 
@@ -33,10 +42,11 @@ public class MainActivity extends AppCompatActivity {
         btnCreateCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                @SuppressLint("SimpleDateFormat") SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日");// HH:mm:ss
-                //获取当前时间
-                Date date = new Date(System.currentTimeMillis());
-                textView.setText(md5(simpleDateFormat.format(date)));
+//                @SuppressLint("SimpleDateFormat") SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日");// HH:mm:ss
+//                //获取当前时间
+//                Date date = new Date(System.currentTimeMillis());
+//                textView.setText(md5(simpleDateFormat.format(date)));
+                test2();
             }
         });
 
@@ -53,6 +63,22 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
+    private void test2(){
+        CodeBean p2 = new CodeBean();
+        p2.setCode("aaaa");
+        p2.setIsuse(false);
+        p2.save(new SaveListener<String>() {
+            @Override
+            public void done(String objectId, BmobException e) {
+                if(e==null){
+                    Toast.makeText(MainActivity.this, "添加数据成功，返回objectId为："+objectId, Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "创建数据失败：" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 
     @NonNull
     public String md5(String string) {
